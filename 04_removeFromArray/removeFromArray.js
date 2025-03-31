feb0dd817e071cc08b96a481995e0b3c89e1c521
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2025-03-31 16:10:50 +0200

    Exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    for (i = 0; i < items.length; i++){
+        // Remove item from array
+        for (j = 0; j < arr.length; j++){
+            if (items[i] === arr[j]){
+                arr.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
