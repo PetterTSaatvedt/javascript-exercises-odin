8c5ce5884ff7bb6ddd97e638d65c4149383b765a
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2025-04-01 12:24:29 +0200

    Exercise 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let temp = (fahrenheit - 32) * 0.55555555555;
+
+  return Number.parseFloat(temp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let temp = celsius * 1.8 + 32;
+
+  return Number.parseFloat(temp.toFixed(1));
 };
 
 // Do not edit below this line
